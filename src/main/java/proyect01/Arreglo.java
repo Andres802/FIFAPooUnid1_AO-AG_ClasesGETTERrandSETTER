3c2beb99d98fe5b8f69494f8fcc0173d5a49edae
ochoa <ochoa@172.16.220.115>
2022-10-20 20:33:58 -0500

    Trabajo3_UNIDAD1_Vectores,Matrices

---
+++ src/main/java/proyect01/Arreglo.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyect01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ochoa
+ */
+public class Arreglo {
+
+    public int[] crearVector(int f) {
+        var retorno = new int[f];
+        var lectura = new Scanner(System.in);
+        for (int i = 0; i < f; i++) {
+            System.out.println("Ingrese número en la posición: " + i);
+            retorno[i] = lectura.nextInt();
+        }
+        return retorno;
+    }
+
+    public int[][] crearMatriz(int f, int c) {
+        var retorno = new int[f][c];
+        var lectura1 = new Scanner(System.in);
+        for (var i = 0; i < f; i++) {
+            for (var j = 0; j < c; j++) {
+                System.out.println("Ingrese el valor correspondiente a la posición " + i + "," + j);
+                retorno[i][j] = lectura1.nextInt();
+            }
+        }
+
+        for (var i = 0; i < f; i++) {
+            for (var j = 0; j < c; j++) {
+                System.out.print(retorno[i][j] + "  ");
+            }
+            System.out.println("  ");
+        }
+
+        return retorno;
+    }
+
+    public int[] sumaVector(int[] v1, int[] v2) {
+        var retorno = new int[v1.length];
+        for (var i = 0; i < retorno.length; i++) {
+            retorno[i] = v1[i] + v2[i];
+        }
+        return retorno;
+    }
+    
+    public int[][] sumaMatriz(int m1 ,int m2){
+        var retorno =new int[m1][m2];
+        for(var i = 0; i<retorno.length;i++){
+            for(var j = 0; j<retorno.length;j++){
+         
+                retorno[i][j] = m1 + m2;
+        }
+        }
+        return retorno;
+    }
+}
